7fb223b384234aa83908eb6dbaa6f843141aa2c7
Drew Northup <drew.northup@maine.edu>
2024-03-29 15:45:43 -0400

    Initial files from professor
    Initial server.c and client.c from Professor Phil
    Dickens.

---
+++ client.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<netdb.h>
+#define IP "141.114.205.43"
+#define PORT 8003
+
+struct sockaddr_in sa ;
+int main()
+        { int err;
+          char my_buf[1024] ;
+          char *msg = "I am your main socket squeeze" ;
+
+         sprintf(my_buf, "%s", "YO BRO!\n") ;
+
+         int my_sock = socket(AF_INET, SOCK_STREAM, 0) ;
+         sa.sin_family   = AF_INET ;
+         sa.sin_port     = htons(PORT) ;
+         inet_pton(AF_INET,IP , &(sa.sin_addr)) ; //fdill in IP address of client
+         printf("BEFORE CONNECT\n") ;
+         int x = connect(my_sock, (struct sockaddr *) &sa, sizeof(sa)) ;
+         if(x < 0)
+		perror("Connet") ;
+
+         printf ("Sending Message to Server!\n") ;
+         send (my_sock, my_buf, 32,  0) ;
+         printf ("Receiving Message from Server!\n") ;
+         int recv_size = recv(my_sock , my_buf , 32 , 0) ;
+         printf("Here is the message from server: \n%s\n", my_buf) ;
+         }
+
---
+++ server.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<netdb.h>
+#define IP "141.114.205.43"
+#define PORT 8003
+
+int send_size, recv_size ;
+int main(int argc , char *argv[])
+{
+    int socket_desc , client_sock , recv_size;
+    socklen_t c ;
+    struct sockaddr_in server , client, my_addr;
+    char client_message[20000];
+
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    client_sock = socket(AF_INET , SOCK_STREAM , 0);
+    
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    inet_pton (AF_INET, IP, &(server.sin_addr)) ; //fill in server ip address
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("bind failed. Error");
+        return 1;
+    }
+   printf("Socket is bound! Waiting for connections!\n") ;
+
+   listen(socket_desc , 1); 
+   client_sock = accept(socket_desc, (struct sockaddr *)&client, &c);    
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+
+    recv_size = recv(client_sock, client_message , 32 , 0) ;
+    printf("Got this from Client %s\n", client_message) ;
+    sprintf(client_message,"How Now MONJO1! Yo.\n") ; 
+    send_size = send(client_sock , client_message , 32, 0);
+    return 0;
+}
+
