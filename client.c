62807f98a554b1088da489c214cb9b286cbbf62e
Drew Northup <drew.northup@maine.edu>
2024-03-29 20:34:06 -0400

    Client formatting fixes, error handling
    In the client:
    * Add error handling code
    * Apply formatting fixes

--- client.c
+++ client.c
@@ -8,27 +8,48 @@
 #define IP "127.0.0.1"
 #define PORT 8255
 
-struct sockaddr_in sa ;
+struct sockaddr_in sa;
 int main()
+{
   //int err;
   char my_buf[1024];
   //char *msg = "I am your main socket squeeze";
 
   snprintf(my_buf, sizeof(my_buf), "%s", "YO BRO!\n");
 
-         int my_sock = socket(AF_INET, SOCK_STREAM, 0) ;
-         sa.sin_family   = AF_INET ;
-         sa.sin_port     = htons(PORT) ;
-         inet_pton(AF_INET,IP , &(sa.sin_addr)) ; //fdill in IP address of client
-         printf("BEFORE CONNECT\n") ;
-         int x = connect(my_sock, (struct sockaddr *) &sa, sizeof(sa)) ;
-         if(x < 0)
-		perror("Connet") ;
-
-         printf ("Sending Message to Server!\n") ;
-         send (my_sock, my_buf, 32,  0) ;
-         printf ("Receiving Message from Server!\n") ;
-         int recv_size = recv(my_sock , my_buf , 32 , 0) ;
-         printf("Here is the message from server: \n%s\n", my_buf) ;
-         }
-
+  int my_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (my_sock==-1)
+  {
+    perror("Error creating socket");
+    exit(EXIT_FAILURE);
+  }
+  
+  sa.sin_family = AF_INET;
+  sa.sin_port = htons(PORT);
+  inet_pton(AF_INET, IP, &(sa.sin_addr)); // fill in IP address of client
+  printf("BEFORE CONNECT\n");
+  int x = connect(my_sock, (struct sockaddr *)&sa, sizeof(sa));
+  if (x < 0)
+  {
+    perror("Connect");
+    exit(EXIT_FAILURE);
+  }
+  printf("Sending Message to Server!\n");
+  int sent = send(my_sock, my_buf, 32, 0);
+  if (sent < 0)
+  {
+    perror("Error sending message");
+    exit(EXIT_FAILURE);
+  }
+  
+  printf("Receiving Message from Server!\n");
+  int recv_size = recv(my_sock, my_buf, 32, 0);
+  if (recv_size==-1)
+  {
+    perror("Error receiving reply");
+    exit(EXIT_FAILURE);
+  }
+  
+  printf("Here is the message from server: \n%s\n", my_buf);
+  exit(EXIT_SUCCESS);
+}
